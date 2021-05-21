efb7747746ebc19d35663fcb537e3e36db157f10
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2021-05-21 23:47:42 +0200

    Database configuration updated to postgis

--- parametrik/parametrik/settings.py
+++ parametrik/parametrik/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = "parametrik.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
